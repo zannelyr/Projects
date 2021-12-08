5fd99fdd553ea70570303d3c45c13311827b13ca
Zannely Rodriguez <zrodriguez@microsoft.com>
2021-12-08 14:41:47 -0800

    added CalorieTracker with entity framework

---
+++ Blazor/CalorieTrackerEF/CalorieTrackerEF/Server/Data/ApplicationDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CalorieTrackerEF.Shared.Models;
+
+namespace CalorieTrackerEF.Server.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+        public DbSet<Developer> Developers { get; set; }
+    }
+}
